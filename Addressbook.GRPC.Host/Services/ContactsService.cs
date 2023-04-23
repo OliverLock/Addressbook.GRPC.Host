81a312b3637a0cff83150ef1cdcfc9ae9f486627
Oliver Lock <oliverlock@ntlworld.com>
2023-04-23 19:41:25 +0100

    Add project files.

---
+++ Addressbook.GRPC.Host/Services/ContactsService.cs
@@ -0,0 +1,29 @@
+﻿using Addressbook.GRPC.Host;
+using Grpc.Core;
+
+namespace Addressbook.GRPC.Host.Services
+{
+    public class ContactsService : Contacts.ContactsBase
+    {
+        private readonly ILogger<ContactsService> _logger;
+        public ContactsService(ILogger<ContactsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AddContactResponse> AddContact(AddContactRequest requestBody, ServerCallContext context)
+        {
+            Random rnd = new Random();
+
+            var response = new AddContactResponse {
+                Message = "Succesfully Added Contact",
+                FirstName = requestBody.FirstName,
+                LastName = requestBody.LastName,
+                Id = rnd.Next()
+            };
+
+            return Task.FromResult(response);
+
+        }
+    }
+}
