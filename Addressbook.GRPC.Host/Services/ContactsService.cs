497c3901f86d5030983a43d236145fe4692b0a84
Oliver Lock <oliverlock@ntlworld.com>
2023-04-23 21:11:46 +0100

    Updated contact to add addational get contact functionality.

--- Addressbook.GRPC.Host/Services/ContactsService.cs
+++ Addressbook.GRPC.Host/Services/ContactsService.cs
@@ -25,5 +25,37 @@ namespace Addressbook.GRPC.Host.Services
             return Task.FromResult(response);
 
         }
+        public override Task<GetContactsResponse> GetContacts(Empty requestBody, ServerCallContext context)
+        {
+            Random rnd = new Random();
+
+            var response = new GetContactsResponse
+            {
+                Message = "Success",
+            };
+            var contact1 = new Contact
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                PhoneNumber = "0123456789",
+                PostCode = "AB12 3CD",
+                Id = 12
+            };
+            response.Contacts.Add(contact1);
+
+            var contact2 = new Contact
+            {
+                FirstName = "Jane",
+                LastName = "Doe",
+                PhoneNumber = "9876543210",
+                PostCode = "AB12 3CD",
+                Id = 13
+            };
+            response.Contacts.Add(contact2);
+
+
+            return Task.FromResult(response);
+
+        }
     }
 }
